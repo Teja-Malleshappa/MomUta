3337dfba0ab14461ec6df13152f76ccfeec6903a
TejaMalleshappa <teja.m@sony.com>
2025-07-12 18:26:04 +0530

    new hero commit

---
+++ src/components/homepage/Constant.js
@@ -0,0 +1,7 @@
+export const navList = [
+    { name: "Swiggy Corporate", link: "https://www.swiggy.com/corporate/" },
+    {
+      name: "Partner with us",
+      link: "https://partner.swiggy.com/login#/swiggy",
+    },
+  ];
