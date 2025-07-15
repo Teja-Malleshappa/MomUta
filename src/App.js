2a4b46b466d8af542a6d5f268ad510a7cf96deab
Teja-Malleshappa <tejam724@gmail.com>
2025-07-15 12:09:30 +0530

    Updated the Winky Rough google font family

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import HomePage from "./components/HomePage";
-import "./fonts.css"
 
 const App = () => {
   return (
-    <div className="w-full h-full font-gilroy">
+    <div className="w-full h-full font-winky">
       <HomePage />
     </div>
   );
